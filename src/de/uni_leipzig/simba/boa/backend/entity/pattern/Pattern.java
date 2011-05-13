580f5d29396808db299b18f64bbdb9d853d22b12
Daniel Gerber <Daniel Gerber@localhost>
2011-05-13 09:59:34 +0200

    withoutlogconfidence  initalized with -1

--- src/de/uni_leipzig/simba/boa/backend/entity/pattern/Pattern.java
+++ src/de/uni_leipzig/simba/boa/backend/entity/pattern/Pattern.java
@@ -105,6 +105,7 @@ public class Pattern extends de.uni_leipzig.simba.boa.backend.persistance.Entity
 		this.learnedFrom = new HashMap<String,Integer>();
 		this.numberOfOccurrences = 1;
 		this.withLogConfidence = -1D;
+		this.withoutLogConfidence = -1D;
 	}
 
 	/**
