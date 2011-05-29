adc0a9718a5ecdf2ca107cc35d1fb5c26c616a00
Daniel Gerber <Daniel Gerber@localhost>
2011-05-29 20:01:38 +0200

    fixed bug with new array initialization

--- src/de/uni_leipzig/simba/boa/backend/configuration/command/impl/CreateKnowledgeCommand.java
+++ src/de/uni_leipzig/simba/boa/backend/configuration/command/impl/CreateKnowledgeCommand.java
@@ -3,6 +3,7 @@ package de.uni_leipzig.simba.boa.backend.configuration.command.impl;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -46,7 +47,7 @@ public class CreateKnowledgeCommand implements Command {
 	private final Property rdfsLabel;
 	private final List<Pattern> patternList = patternDao.findAllPatterns();
 	private Model model;
-	private List<String> statementList;
+	private List<String> statementList = new ArrayList<String>();
 	
 	public CreateKnowledgeCommand () {
 		
@@ -130,12 +131,16 @@ public class CreateKnowledgeCommand implements Command {
 //										model.addStatement(typeLeft);
 //										model.addStatement(typeRight);
 										
-										out.append("LeftLabel(Domain):\t" + leftLabel + Constants.NEW_LINE_SEPARATOR);
-										out.append("Pattern: " + pattern.getNaturalLanguageRepresentation() + Constants.NEW_LINE_SEPARATOR);
-										out.append("RightLabel(Range):\t" + rightLabel + Constants.NEW_LINE_SEPARATOR);
-										out.append("Statement created: " + link.toString() + Constants.NEW_LINE_SEPARATOR);
-										out.append(Constants.NEW_LINE_SEPARATOR);
-										System.out.println("Statement created: " + link);
+										if ( !statementList.contains(link.toString()) ) {
+											
+											statementList.add(link.toString());
+											out.append("LeftLabel(Domain):\t" + leftLabel + Constants.NEW_LINE_SEPARATOR);
+											out.append("Pattern: " + pattern.getNaturalLanguageRepresentation() + Constants.NEW_LINE_SEPARATOR);
+											out.append("RightLabel(Range):\t" + rightLabel + Constants.NEW_LINE_SEPARATOR);
+											out.append("Statement created: " + link.toString() + Constants.NEW_LINE_SEPARATOR);
+											out.append(Constants.NEW_LINE_SEPARATOR);
+											System.out.println("Statement created: " + link);
+										}
 									}
 								}
 								else {
