0670b56782ea89086909966d2c573d1f15579d0a
Daniel Gerber <Daniel Gerber@localhost>
2011-07-28 21:04:10 +0200

    changed creation of trained sentences to one file a class for all classes with over 10k hits

--- src/de/uni_leipzig/simba/boa/backend/nlp/learn/NamedEntityRecognizerLearner.java
+++ src/de/uni_leipzig/simba/boa/backend/nlp/learn/NamedEntityRecognizerLearner.java
@@ -6,8 +6,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.Map.Entry;
@@ -28,6 +32,7 @@ import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.util.FileManager;
 
+import de.danielgerber.string.StringUtil;
 import de.uni_leipzig.simba.boa.backend.configuration.NLPediaSettings;
 import de.uni_leipzig.simba.boa.backend.logging.NLPediaLogger;
 import de.uni_leipzig.simba.boa.backend.util.SerializationUtil;
@@ -54,7 +59,10 @@ public class NamedEntityRecognizerLearner {
 	private String pathToTypesFile = PREFIX + "instance_types_en.nt";
 	private String pathToDBpediaOntology = PREFIX + "dbpedia_3.6.owl";
 	private String pathToSerializedFile = PREFIX + "knowledge.ser";
-
+	
+	// top 41 classes with more than 10000 occurrences
+	private List<String> types = Arrays.asList(new String[]{"Writer","Airport","MilitaryUnit","Mountain","University","Fish","Bird","Planet","BaseballPlayer","SoccerClub","City","AutomobileEngine","Road","RadioStation","VideoGame","MilitaryPerson","TelevisionShow","River","OfficeHolder","Ship","Book","School","Band","Mollusca","HistoricPlace","MusicalArtist","Plant","Company","PersonFunction","AdministrativeRegion","Town","Person","Single","Actor","Insect","Film","SoccerPlayer","Village","TelevisionEpisode","Album","Settlement"}); 
+	
 	private int maxNumberOfDocuments = Integer.valueOf(NLPediaSettings.getInstance().getSetting("maxNumberOfLearnSentences"));
 
 	/**
@@ -138,31 +146,34 @@ public class NamedEntityRecognizerLearner {
 					
 					if ( label != null  && !label.isEmpty() ) {
 						
-						// find all sentences containing the label
-						Map<Integer, String> sentencesContainingLabels = getSentencesContainingLabel(label);
-						
 						String typeReplacement = getTypeForUri(uri, type);
 						typeReplacement = typeReplacement.replace("http://dbpedia.org/ontology/", "");
-
-						String[] tokensOfLabel = label.split(" ");
-
-						for (Entry<Integer, String> sent : sentencesContainingLabels.entrySet()) {
-
-							int indexId = sent.getKey();
-							// if the sentence was already found in the sentence
-							// list, take it from there else use it untagged
-							// from the index
-							String sentence = sent.getValue();
-							if (sentences.containsKey(indexId)) sentences.get(indexId);
-
-							String replacement = "";
-							for (int i = 0; i < tokensOfLabel.length; i++) {
-								
-								replacement += tokensOfLabel[i] + "___" + typeReplacement + " ";
-							}
+						
+						if ( this.types.contains(typeReplacement) ) {
 							
-							// replace the whole label with the complete replacement
-							sentences.put(indexId, sentence.replace(label, replacement));
+							// find all sentences containing the label
+							Map<Integer, String> sentencesContainingLabels = getSentencesContainingLabel(label);
+							
+							String[] tokensOfLabel = label.split(" ");
+
+							for (Entry<Integer, String> sent : sentencesContainingLabels.entrySet()) {
+
+								int indexId = sent.getKey();
+								// if the sentence was already found in the sentence
+								// list, take it from there else use it untagged
+								// from the index
+								String sentence = sent.getValue();
+								if (sentences.containsKey(indexId)) sentences.get(indexId);
+
+								String replacement = "";
+								for (int i = 0; i < tokensOfLabel.length; i++) {
+									
+									replacement += tokensOfLabel[i] + "___" + typeReplacement + " ";
+								}
+								
+								// replace the whole label with the complete replacement
+								sentences.put(indexId, sentence.replace(label, replacement));
+							}
 						}
 					}
 				}
@@ -178,40 +189,73 @@ public class NamedEntityRecognizerLearner {
 	private void writeTrainedModelToFile() {
 
 		try {
-
-			// write the trained model in utf8 to a file containing
-			// "word \tab tag" per line
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(pathToTrainedSentenceFile)), "UTF-8"));
-
-			System.out.println("\nWriting " + sentences.values().size() + " sentences to file!");
-
+			
+			// create a writer for every type
+			Map<String,BufferedWriter> writers = new HashMap<String,BufferedWriter>();
+			for (String type : types) {
+				
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(pathToTrainedSentenceFile + "." + type)), "UTF-8"));
+				writers.put(type, writer);
+			}
+			System.out.println("There are " + types.size() + " different classes or writers resp. in th map.");
+			
 			for (String sentence : sentences.values()) {
+				
+				String tag = this.getFavoredTag(StringUtil.getRegexMatches(sentence, "___[a-zA-Z]*\\s"));
 
+				Writer w = writers.get(tag);
+				
 				for (String token : sentence.split(" ")) {
 
 					if ( !token.isEmpty() ) {
 
 						if (!token.contains("___")) {
 
-							writer.write(token + "\t" + "O" + System.getProperty("line.separator"));
+							w.write(token + "\t" + "O" + System.getProperty("line.separator"));
 						}
 						else {
 
 							String[] tokens = token.split("___");
 							if (tokens.length == 2) {
 								
-								writer.write(tokens[0] + "\t" + tokens[1] + System.getProperty("line.separator"));
+								w.write(tokens[0] + "\t" + tokens[1] + System.getProperty("line.separator"));
 							}
 						}
 					}
 				}
 			}
-			writer.close();
+			for ( BufferedWriter writer : writers.values() ) writer.close();
 		}
 		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	private String getFavoredTag(List<String> regexMatches) {
+
+		Map<String,Integer> counts =  new HashMap<String,Integer>();
+		
+		for (int i = 0; i < regexMatches.size() ; i++) {
+			
+			String s = regexMatches.get(i);
+			if ( counts.containsKey(s) ) {
+				
+				counts.put(s, counts.get(s) + 1);
+			}
+			else counts.put(s, 1);
+		}
+		String max = "";
+		int maxOcc = -1;
+		for (Map.Entry<String, Integer> pair : counts.entrySet()) {
+			
+			if ( pair.getValue() >= maxOcc ) {
+				
+				max = pair.getKey();
+				maxOcc = pair.getValue();
+			}
+		}
+		return max;
 	}
 
 	private String getTypeForUri(String uri, Set<String> types) {
@@ -304,4 +348,19 @@ public class NamedEntityRecognizerLearner {
         bar.append("]   " + percent + "%     ");
         System.out.print("\r" + bar.toString());
     }
+	
+//	public static void main(String[] args) {
+		//
+//				List<String> test = new ArrayList<String>();
+//				test.add("___AAA");
+//				test.add("___AAA");
+//				test.add("___AAA");
+//				test.add("___CCC");
+//				test.add("___DDD");
+//				test.add("___DDD");
+//				test.add("___BBB");
+//				test.add("___BBB");
+//				
+//				System.out.println(getFavoredTag(test));
+//			}
 }
