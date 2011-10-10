502a0c9ed25a60e825efae8cba6a5ddea8f0cae1
gerbsen <gerbsen@localhost>
2011-10-10 08:07:56 +0200

    knowledge know without issubject/object

--- src/de/uni_leipzig/simba/boa/backend/configuration/command/impl/WriteRelationToFileCommand.java
+++ src/de/uni_leipzig/simba/boa/backend/configuration/command/impl/WriteRelationToFileCommand.java
@@ -25,14 +25,11 @@ public class WriteRelationToFileCommand implements Command {
 
 	private final String SPARQL_ENDPOINT_URI	= NLPediaSettings.getInstance().getSetting("dbpediaSparqlEndpoint");
 	private final String DBPEDIA_DEFAULT_GRAPH	= NLPediaSettings.getInstance().getSetting("dbpediaDefaultGraph");
-	private final String LABEL_OUTPUT_FILE		= NLPediaSettings.getInstance().getSetting("labelOutputFile");
-	private static final int LIMIT				= 100000;
+	private static final int LIMIT				= 25000;
 	
 	private final NLPediaLogger logger = new NLPediaLogger(WriteRelationToFileCommand.class);
 	
 	private static final String language = "en";
-	
-	private enum Type { Subject, Object }; 
 	
 	public static void main(String[] args) {
 
@@ -46,167 +43,61 @@ public class WriteRelationToFileCommand implements Command {
 	 */
 	public void execute() {
 		
-//		String queryPersonSubject		= createQueryPersonSubject();
-//		String queryPersonObject		= this.createQueryPersonObject();
-//		getKnowledge(queryPersonSubject, "/Users/gerb/en.txt", Type.Subject);
-//		getKnowledge(queryPersonObject, "/Users/gerb/en.txt", Type.Object);
-//		
-//		String queryPlaceSubject		= this.createQueryPlaceSubject();
-//		String queryPlaceObject			= this.createQueryPlaceObject();
-//		getKnowledge(queryPlaceSubject, "/Users/gerb/en.txt", Type.Subject);
-//		getKnowledge(queryPlaceObject, "/Users/gerb/en.txt", Type.Object);
-//		
-//		String queryOrganisationSubject	= this.createQueryOrganisationSubject();
-		String queryOrganisationObject	= this.createQueryOrganisationObject();
-//		getKnowledge(queryOrganisationSubject, "/Users/gerb/en.txt", Type.Subject);
-		getKnowledge(queryOrganisationObject, "/Users/gerb/en.txt", Type.Object);
+		String queryPersonSubject		= this.createQuerySubject("http://dbpedia.org/ontology/Person");
+		String queryPersonObject		= this.createQueryObject("http://dbpedia.org/ontology/Person");
+		getKnowledge(queryPersonSubject, "/home/gerber/en_new.txt");
+		getKnowledge(queryPersonObject, "/home/gerber/en_new.txt");
+		
+		String queryPlaceSubject		= this.createQuerySubject("http://dbpedia.org/ontology/Place");
+		String queryPlaceObject			= this.createQueryObject("http://dbpedia.org/ontology/Place");
+		getKnowledge(queryPlaceSubject, "/home/gerber/en_new.txt");
+		getKnowledge(queryPlaceObject, "/home/gerber/en_new.txt");
+		
+		String queryOrganisationSubject	= this.createQuerySubject("http://dbpedia.org/ontology/Organisation");
+		String queryOrganisationObject	= this.createQueryObject("http://dbpedia.org/ontology/Organisation");
+		getKnowledge(queryOrganisationSubject, "/home/gerber/en_new.txt");
+		getKnowledge(queryOrganisationObject, "/home/gerber/en_new.txt");
 	}
 
-	private String createQueryOrganisationObject() {
+	private String createQueryObject(String typeUri) {
 
 		return 
 			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
 			"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  " +
-			"SELECT ?s2 ?s2l ?p2 ?o2 ?o2l ?rangep2 ?domainp2 " +
+			"SELECT ?s ?sl ?p ?o ?ol ?range ?domain " +
 			"WHERE {" +
-			 "	?s2 rdf:type <http://dbpedia.org/ontology/Organisation> ." +
-			 "  ?s2 rdfs:label ?s2l . " +
-			 "  ?o2 ?p2 ?s2 . " +
-			 "	?o2 rdfs:label ?o2l " +
-			 "	FILTER (  langMatches( lang(?s2l), \""+language+"\" )  && langMatches( lang(?o2l), \""+language+"\" ) ) " +
-			 "	?p2  rdfs:range  ?rangep2 . " +
-			 "	?p2  rdfs:domain ?domainp2 . " +
+			 "	?s rdfs:label ?sl . " + 
+			 "  ?s ?p ?o . " +
+			 "	?o rdf:type <"+typeUri+"> . " +
+			 "  ?o rdfs:label ?ol . " +
+			 "	FILTER (  langMatches( lang(?sl), \""+language+"\" )  && langMatches( lang(?ol), \""+language+"\" ) ) " +
+			 "	?p rdfs:range  ?range . " +
+			 "	?p rdfs:domain ?domain . " +
 			 "} " +
 			 "LIMIT " + LIMIT +  " " +
 			 "OFFSET &OFFSET";
 	}
 
-	private String createQueryOrganisationSubject() {
+	private String createQuerySubject(String typeUri) {
 
 		return 
 			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
 			"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  " +
-			"SELECT ?s1 ?s1l ?p1 ?o1 ?o1l ?rangep1 ?domainp1 " +
+			"SELECT ?s ?sl ?p ?o ?ol ?range ?domain " +
 			"WHERE {" +
-			 "	?s1 rdf:type <http://dbpedia.org/ontology/Organisation> . " +
-			 "  ?s1 rdfs:label ?s1l . " +
-			 "	?s1 ?p1 ?o1 . " +
-			 "	?o1 rdfs:label ?o1l ." +
-			 "	FILTER (  langMatches( lang(?s1l), \""+language+"\" )  && langMatches( lang(?o1l), \""+language+"\" ) ) " +
-			 "	?p1  rdfs:range  ?rangep1 . " +
-			 "	?p1  rdfs:domain ?domainp1 . " +
+			 "	?s rdf:type <"+typeUri+"> . " +
+			 "  ?s rdfs:label ?sl . " +
+			 "	?s ?p ?o . " +
+			 "	?o rdfs:label ?ol ." +
+			 "	FILTER (  langMatches( lang(?sl), \""+language+"\" )  && langMatches( lang(?ol), \""+language+"\" ) ) " +
+			 "	?p  rdfs:range  ?range . " +
+			 "	?p  rdfs:domain ?domain . " +
 			 "} " +
 			 "LIMIT " + LIMIT +  " " +
 			 "OFFSET &OFFSET";
 	}
 
-	private String createQueryPlaceObject() {
-
-		return
-			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-			"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  " +
-			"SELECT ?s2 ?s2l ?p2 ?o2 ?o2l ?rangep2 ?domainp2 " +
-			"WHERE {" +
-			 "	?s2 rdf:type <http://dbpedia.org/ontology/Place> ." +
-			 "  ?s2 rdfs:label ?s2l . " +
-			 "  ?o2 ?p2 ?s2 . " +
-			 "	?o2 rdfs:label ?o2l " +
-			 "	FILTER (  langMatches( lang(?s2l), \""+language+"\" )  && langMatches( lang(?o2l), \""+language+"\" ) ) " +
-			 "	?p2  rdfs:range  ?rangep2 . " +
-			 "	?p2  rdfs:domain ?domainp2 . " +
-			 "} " +
-			 "LIMIT " + LIMIT +  " " +
-			 "OFFSET &OFFSET";
-	}
-
-	private String createQueryPlaceSubject() {
-
-		return 
-			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-			"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  " +
-			"SELECT ?s1 ?s1l ?p1 ?o1 ?o1l ?rangep1 ?domainp1 " +
-			"WHERE {" +
-			 "	?s1 rdf:type <http://dbpedia.org/ontology/Place> . " +
-			 "  ?s1 rdfs:label ?s1l . " +
-			 "	?s1 ?p1 ?o1 . " +
-			 "	?o1 rdfs:label ?o1l ." +
-			 "	FILTER (  langMatches( lang(?s1l), \""+language+"\" )  && langMatches( lang(?o1l), \""+language+"\" ) ) " +
-			 "	?p1  rdfs:range  ?rangep1 . " +
-			 "	?p1  rdfs:domain ?domainp1 . " +
-			 "} " +
-			 "LIMIT " + LIMIT +  " " +
-			 "OFFSET &OFFSET";
-	}
-
-	private String createQueryPersonObject() {
-
-		return 
-			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-			"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  " +
-			"SELECT ?s2 ?s2l ?p2 ?o2 ?o2l ?rangep2 ?domainp2 " +
-			"WHERE {" +
-			 "	?s2 rdf:type <http://dbpedia.org/ontology/Person> ." +
-			 "  ?s2 rdfs:label ?s2l . " +
-			 "  ?o2 ?p2 ?s2 . " +
-			 "	?o2 rdfs:label ?o2l " +
-			 "	FILTER (  langMatches( lang(?s2l), \""+language+"\" )  && langMatches( lang(?o2l), \""+language+"\" ) ) " +
-			 "	?p2  rdfs:range  ?rangep2 . " +
-			 "	?p2  rdfs:domain ?domainp2 . " +
-			 "} " +
-			 "LIMIT " + LIMIT +  " " +
-			 "OFFSET &OFFSET";
-	}
-
-	private static String createQueryPersonSubject() {
-
-		return 
-			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-			"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  " +
-			"SELECT ?s1 ?s1l ?p1 ?o1 ?o1l ?rangep1 ?domainp1 " +
-			"WHERE {" +
-			 "	?s1 rdf:type <http://dbpedia.org/ontology/Person> . " +
-			 "  ?s1 rdfs:label ?s1l . " +
-			 "	?s1 ?p1 ?o1 . " +
-			 "	?o1 rdfs:label ?o1l ." +
-			 "	FILTER (  langMatches( lang(?s1l), \""+language+"\" )  && langMatches( lang(?o1l), \""+language+"\" ) ) " +
-			 "	?p1  rdfs:range  ?rangep1 . " +
-			 "	?p1  rdfs:domain ?domainp1 . " +
-			"} " +
-			"LIMIT " + LIMIT +  " " +
-			"OFFSET &OFFSET";
-	}
-
-//	private void getPlaceObjectKnowledge(String queryPlaceObject) {
-//
-//		ResultSet resultSet = this.getResultSet(queryPlaceObject);
-//		this.handleObjectQuery("/Users/gerb/place_object.txt", resultSet);
-//	}
-//
-//	private void getPlaceSubjectKnowledge(String queryPlaceSubject) {
-//
-//		ResultSet resultSet = this.getResultSet(queryPlaceSubject);
-//		this.handleSubjectQuery("/Users/gerb/place_subject.txt", resultSet);
-//	}
-//
-//	private void getOrganisationObjectKnowledge(String queryOrganisationObject) {
-//
-//		ResultSet resultSet = this.getResultSet(queryOrganisationObject);
-//		this.handleObjectQuery("/Users/gerb/organisation_object.txt", resultSet);
-//	}
-//
-//	private void getOrganisationSubjectKnowledge(String queryOrganisationSubject) {
-//
-//		ResultSet resultSet = this.getResultSet(queryOrganisationSubject);
-//		this.handleSubjectQuery("/Users/gerb/organisation_subject.txt", resultSet);
-//	}
-//
-//	private void getPersonObjectKnowledge(String queryPersonObject) {
-//
-//		ResultSet resultSet = this.getResultSet(queryPersonObject);
-//		this.handleObjectQuery("/Users/gerb/person_object.txt", resultSet);
-//	}
-
-	private void handleSubjectQuery(String fileName, List<QuerySolution> resultSets) {
+	private void handleQuery(String fileName, List<QuerySolution> resultSets) {
 
 		Writer writer = null;
 		
@@ -216,25 +107,26 @@ public class WriteRelationToFileCommand implements Command {
 			
 			for (QuerySolution solution : resultSets) {
 				
-				if (solution.get("s1") != null && solution.get("p1") != null && solution.get("o1") != null) {
+				if (solution.get("s") != null && solution.get("p") != null && solution.get("o") != null) {
 	
-					if (solution.get("o1l") != null) {
+					if (solution.get("ol") != null) {
 	
-						String s1l = solution.get("s1l").toString(), o1l = solution.get("o1l").toString();
+						String sl = solution.get("sl").toString();
+						String ol = solution.get("ol").toString();
 	
-						if (o1l.contains("@")) {
+						if (ol.contains("@")) {
 	
-							o1l = o1l.substring(0, o1l.lastIndexOf("@"));
+							ol = ol.substring(0, ol.lastIndexOf("@"));
 						}
-						if (s1l.contains("@")) {
+						if (sl.contains("@")) {
 	
-							s1l = s1l.substring(0, s1l.lastIndexOf("@"));
+							sl = sl.substring(0, sl.lastIndexOf("@"));
 						}
 						
-						String range = solution.get("rangep1") == null ? "null" : solution.get("rangep1").toString();
-						String domain = solution.get("domainp1") == null ? "null" : solution.get("domainp1").toString();
+						String range = solution.get("range") == null ? "null" : solution.get("range").toString();
+						String domain = solution.get("domain") == null ? "null" : solution.get("domain").toString();
 						
-						writer.write(solution.get("s1").toString() + " ||| " + s1l + " ||| " + solution.get("p1").toString() + " ||| " + solution.get("o1").toString() + " ||| " + o1l + " ||| " + range + " ||| " + domain + " ||| isSubject");
+						writer.write(solution.get("s").toString() + " ||| " + sl + " ||| " + solution.get("p").toString() + " ||| " + solution.get("o").toString() + " ||| " + ol + " ||| " + range + " ||| " + domain);
 						writer.write(System.getProperty("line.separator"));
 					}
 				}
@@ -259,61 +151,7 @@ public class WriteRelationToFileCommand implements Command {
 		}
 	}
 	
-	private void handleObjectQuery(String fileName, List<QuerySolution> resultSets) {
-
-		
-		Writer writer = null;
-		
-		try {
-			
-			writer =  new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
-			
-			for (QuerySolution solution : resultSets) {
-
-				if (solution.get("s2") != null && solution.get("p2") != null && solution.get("o2") != null) {
-					
-					if (solution.get("o2l") != null) {
-	
-						String s2l = solution.get("s2l").toString(), o2l = solution.get("o2l").toString();
-	
-						if (o2l.contains("@")) {
-	
-							o2l = o2l.substring(0, o2l.lastIndexOf("@"));
-						}
-						if (s2l.contains("@")) {
-	
-							s2l = s2l.substring(0, s2l.lastIndexOf("@"));
-						}
-						
-						String range = solution.get("rangep2") == null ? "null" : solution.get("rangep2").toString();
-						String domain = solution.get("domainp2") == null ? "null" : solution.get("domainp2").toString();
-						
-						writer.write(solution.get("s2").toString() + " ||| " + s2l + " ||| " + solution.get("p2").toString() + " ||| " + solution.get("o2").toString() + " ||| " + o2l + " ||| " + range + " ||| " + domain + " ||| isObject");
-						writer.write(System.getProperty("line.separator"));
-					}
-				}
-			}
-		}
-		catch (Exception e) {
-			
-			e.printStackTrace();
-			logger.error("Could not write to label relation file.", e);
-		}
-		finally {
-	
-			try {
-				
-				writer.close();
-			}
-			catch (IOException ioe) {
-				
-				logger.error("Could not close file writer.", ioe);
-				ioe.printStackTrace();
-			}
-		}
-	}
-
-	private void getKnowledge(String query, String fileName, Type type) {
+	private void getKnowledge(String query, String fileName) {
 		
 		logger.info("Querying started for query: " + query);
 		long start = System.currentTimeMillis();
@@ -334,14 +172,7 @@ public class WriteRelationToFileCommand implements Command {
 			
 			if ( !resultSetList.isEmpty() ) {
 
-				if ( type == Type.Subject ) {
-
-					this.handleSubjectQuery(fileName, resultSetList);
-				}
-				else {
-					
-					this.handleObjectQuery(fileName, resultSetList);
-				}
+				this.handleQuery(fileName, resultSetList);
 			}
 			else break;
 		}
