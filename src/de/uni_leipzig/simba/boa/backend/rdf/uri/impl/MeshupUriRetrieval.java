b811056577fc12a051e85f28a6988592f590b9ef
gerbsen <gerbsen@localhost>
2011-12-10 23:14:03 +0100

    hopefully fixed httpexcpetion in urimeshupretrieval

--- src/de/uni_leipzig/simba/boa/backend/rdf/uri/impl/MeshupUriRetrieval.java
+++ src/de/uni_leipzig/simba/boa/backend/rdf/uri/impl/MeshupUriRetrieval.java
@@ -3,11 +3,14 @@ package de.uni_leipzig.simba.boa.backend.rdf.uri.impl;
 import com.hp.hpl.jena.query.QueryParseException;
 
 import nlpbox.lookup.SparqlLookup;
+import de.uni_leipzig.simba.boa.backend.logging.NLPediaLogger;
 import de.uni_leipzig.simba.boa.backend.rdf.uri.UriRetrieval;
 
 
 public class MeshupUriRetrieval implements UriRetrieval {
 
+	private NLPediaLogger logger = new NLPediaLogger(MeshupUriRetrieval.class);
+	
 	@Override
 	public String getUri(String label) {
 
@@ -19,10 +22,17 @@ public class MeshupUriRetrieval implements UriRetrieval {
 		}
 		catch ( NullPointerException npe) {
 			
+			logger.error("some nullpointer!", npe);
 			uri = "http://boa.akws.org/resource/"+label.replaceAll(" ", "_");
 		}
 		catch ( QueryParseException qpe ) {
 			
+			logger.error("remote lucene index throws parse excpetion because of illegal charactes!", qpe);
+			uri = "http://boa.akws.org/resource/"+label.replaceAll(" ", "_");
+		}
+		catch (Exception e) {
+			
+			logger.error("sometimes we get an httpexception!", e);
 			uri = "http://boa.akws.org/resource/"+label.replaceAll(" ", "_");
 		}
 		if ( uri.startsWith("http://scms.eu/") ) uri = uri.replace("http://scms.eu/", "http://boa.akws.org/resource/");
