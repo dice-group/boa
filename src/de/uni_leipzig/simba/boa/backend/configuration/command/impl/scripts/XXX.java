65dc75005957300aeaf45d17505d639658cd7992
gerbsen <gerbsen@localhost>
2011-12-18 11:44:31 +0100

    index statistics

--- src/de/uni_leipzig/simba/boa/backend/configuration/command/impl/scripts/XXX.java
+++ src/de/uni_leipzig/simba/boa/backend/configuration/command/impl/scripts/XXX.java
@@ -58,10 +58,10 @@ public class XXX implements Command {
 //		String[] indexDirs = new String[]{"/Users/gerb/Development/workspaces/experimental/en_wiki_exp/index/stanfordnlp"};
 		
 		String[] indexDirs = new String[]{	
-				"/home/gerber/nlpedia-data/en_wiki_exp/index/stanfordnlp"
+				"/home/gerber/nlpedia-data/en_wiki_exp/index/stanfordnlp",
 //				"/home/gerber/nlpedia-data/de_news_exp/index/stanfordnlp",
 //				"/home/gerber/nlpedia-data/en_news_exp/index/stanfordnlp"
-//				"/home/gerber/nlpedia-data/de_wiki_exp/index/stanfordnlp",
+				"/home/gerber/nlpedia-data/de_wiki_exp/index/stanfordnlp"
 				};
 		
 		try {
