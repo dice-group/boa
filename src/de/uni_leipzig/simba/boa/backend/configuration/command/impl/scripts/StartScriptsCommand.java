0bea7e3da37753299e23e58e953ba139b8eb0769
gerbsen <gerbsen@localhost>
2012-01-25 13:28:10 +0900

    cleaned scripts src is error free again

--- src/de/uni_leipzig/simba/boa/backend/configuration/command/impl/scripts/StartScriptsCommand.java
+++ src/de/uni_leipzig/simba/boa/backend/configuration/command/impl/scripts/StartScriptsCommand.java
@@ -25,15 +25,12 @@ public class StartScriptsCommand implements Command {
 		    	try {
 		    		
 		    		System.out.println("\t\t0. Go back");
-		    		System.out.println("\t\t1. Create sentence per line file");
-		    		System.out.println("\t\t2. Ask DBpedia for triples");
-		    		System.out.println("\t\t3. Reset pattern confidence measures");
-		    		System.out.println("\t\t4. Create learned from distribution");
-		    		System.out.println("\t\t5. Train NER model");
-		    		System.out.println("\t\t6. Create pos distirbution");
-		    		System.out.println("\t\t7. Create pattern index");
-		    		System.out.println("\t\t8. Create machine learning input");
-		    		System.out.println("\t\t9. XXX");
+		    		System.out.println("\t\t2. Reset pattern confidence measures");
+		    		System.out.println("\t\t3. Create learned from distribution");
+		    		System.out.println("\t\t4. Create pos distirbution");
+		    		System.out.println("\t\t5. Create pattern index");
+		    		System.out.println("\t\t6. Create machine learning input");
+		    		System.out.println("\t\t7. XXX");
 		    		System.out.println("\t\t");
 		    		System.out.println("");
 		    		
@@ -50,58 +47,37 @@ public class StartScriptsCommand implements Command {
 							goBack = true;
 							break;
 					
-						case 1: // Take a plain text input file and detect sentences and write them to a file one per line
-							
-							String input = NLPediaSettings.getInstance().getSetting("rawDataInputDirectory");
-							String ouput = NLPediaSettings.getInstance().getSetting("sentencePerLineOutputFile");
-							
-							Command plainTextToSentencePerLineCommand = new PlainTextToSentencePerLineCommand(input, ouput);
-							plainTextToSentencePerLineCommand.execute();
-							break;
-							
-						case 2: // look if generated triples are present in dbpedia
-							
-							Command askDbpediaCommand = new AskDbpediaForTriple();
-							askDbpediaCommand.execute();
-							break;
-						
-						case 3: // look if generated triples are present in dbpedia
+						case 1: // look if generated triples are present in dbpedia
 							
 							Command resetPatternCommand = new ResetPatternCommand();
 							resetPatternCommand.execute();
 							break;
 							
-						case 4:
+						case 2:
 							
 							Command createLearndFromDistributionCommand = new CreateLearndFromDistributionCommand();
 							createLearndFromDistributionCommand.execute();
 							break;
 							
-						case 5:
-							
-							Command trainNerModelCommand = new TrainNerModelCommand();
-							trainNerModelCommand.execute();
-							break;
-							
-						case 6:
+						case 3:
 							
 							Command posDistributionCommand = new PosDistributionCommand();
 							posDistributionCommand.execute();
 							break;	
 					
-						case 7:
+						case 4:
 							
 							Command createQaIndexCommand = new CreateQuestionAnsweringIndexCommand();
 							createQaIndexCommand.execute();
 							break;
 							
-						case 8:
+						case 5:
 							
 							Command createMLCommand = new CreateMachineLearningCommand();
 							createMLCommand.execute();
 							break;
 						
-						case 9:
+						case 6:
 							
 							Command xxx = new XXX();
 							xxx.execute();
