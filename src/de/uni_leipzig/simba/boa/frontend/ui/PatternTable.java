820ebbfe1a3459c3bc1239feac1c978ae09ef32a
Daniel Gerber <dgerber@informatik.uni-leipzig.de>
2012-11-11 10:28:17 -0500

    score added again to pattern table and sorted by the score

--- src/de/uni_leipzig/simba/boa/frontend/ui/PatternTable.java
+++ src/de/uni_leipzig/simba/boa/frontend/ui/PatternTable.java
@@ -30,7 +30,7 @@ public class PatternTable extends Table {
 		List<Feature> featureList = new ArrayList<Feature>(patterns.get(0).getFeatures().keySet());
 		Collections.sort(featureList, new FeatureNameComparator());
 		
-//		this.addContainerProperty("Score",                            Double.class, null);
+		this.addContainerProperty("Score",                            Double.class, null);
 		this.addContainerProperty("Occurrence",                       Integer.class, null);
 		this.addContainerProperty("Natural Language Representation",  String.class, null);
 		this.addContainerProperty("Generalized",                      String.class, null);
@@ -47,7 +47,7 @@ public class PatternTable extends Table {
         for (Pattern pattern : patterns) {
             
             List<Object> entries = new ArrayList<Object>();
-//            entries.add(pattern.getScore());
+            entries.add(pattern.getScore());
             entries.add(pattern.getNumberOfOccurrences());
             entries.add(pattern.getNaturalLanguageRepresentation());
             entries.add(pattern.getGeneralizedPattern());
@@ -59,7 +59,7 @@ public class PatternTable extends Table {
         }
         
 //        setSortContainerPropertyId(WordUtils.capitalize("SUPPORT_NUMBER_OF_PAIRS_LEARNED_FROM".replace("_", " ").toLowerCase()));
-        setSortContainerPropertyId("Occurrence");
+        setSortContainerPropertyId("Score");
         setSortAscending(false);
         sort();
         setColumnReorderingAllowed(true);
