3f17a7c0312b53bf21707babe41f61681c33d136
Daniel Gerber <dgerber@informatik.uni-leipzig.de>
2013-03-15 11:35:51 +0100

    npe fix in reverb

--- src/de/uni_leipzig/simba/boa/backend/search/impl/DefaultPatternSearcher.java
+++ src/de/uni_leipzig/simba/boa/backend/search/impl/DefaultPatternSearcher.java
@@ -429,7 +429,7 @@ public class DefaultPatternSearcher implements PatternSearcher {
 
         try {
             
-            this.indexSearcher.close();
+        	if ( this.indexSearcher != null ) this.indexSearcher.close();
         }
         catch (IOException e) {
             
