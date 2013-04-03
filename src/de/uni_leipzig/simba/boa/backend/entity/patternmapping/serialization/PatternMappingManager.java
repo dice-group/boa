6ac9fa33043d5cdfaff1478fa7221308210d42f4
Daniel Gerber <dgerber@informatik.uni-leipzig.de>
2013-04-03 17:42:11 +0200

    testing

--- src/de/uni_leipzig/simba/boa/backend/entity/patternmapping/serialization/PatternMappingManager.java
+++ src/de/uni_leipzig/simba/boa/backend/entity/patternmapping/serialization/PatternMappingManager.java
@@ -4,14 +4,21 @@
 package de.uni_leipzig.simba.boa.backend.entity.patternmapping.serialization;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+
+import org.apache.commons.collections.list.TreeList;
 
 import de.uni_leipzig.simba.boa.backend.Constants;
 import de.uni_leipzig.simba.boa.backend.configuration.NLPediaSettings;
 import de.uni_leipzig.simba.boa.backend.entity.pattern.Pattern;
+import de.uni_leipzig.simba.boa.backend.entity.pattern.comparator.PatternNlrComparator;
 import de.uni_leipzig.simba.boa.backend.entity.patternmapping.PatternMapping;
 import de.uni_leipzig.simba.boa.backend.logging.NLPediaLogger;
 import de.uni_leipzig.simba.boa.backend.persistance.serialization.SerializationManager;
@@ -30,11 +37,50 @@ public final class PatternMappingManager {
     private final String PATTERN_MAPPING_FOLDER = NLPediaSettings.BOA_DATA_DIRECTORY + "patternmappings/";
     private final String DEFAULT_DATABASE       = NLPediaSettings.BOA_DATA_DIRECTORY.replaceAll("/$", "");
     
+    
+    public static void main(String[] args) {
+		
+    	Map<String,List<String>> patterns = new TreeMap<String,List<String>>();
+    	Set<PatternMapping> mappings = SerializationManager.getInstance().
+    			deserializePatternMappings("/Users/gerb/Development/workspaces/experimental/boa/qa/en/patternmappings/");
+		for ( PatternMapping mapping : mappings ) {
+    		
+    		for ( Pattern p : mapping.getPatterns()) {
+    			
+    			Matcher matcher = java.util.regex.Pattern.compile("\\d").matcher(p.getNaturalLanguageRepresentation());
+    			Matcher year = java.util.regex.Pattern.compile("\\d{4}").matcher(p.getNaturalLanguageRepresentation());
+    			Matcher rest = java.util.regex.Pattern.compile("\\d{1,2}").matcher(p.getNaturalLanguageRepresentation());
+    			
+				if ( matcher.find() ) {
+					
+					if ( !patterns.containsKey(mapping.getProperty().getUri()) ) patterns.put(mapping.getProperty().getUri(), new ArrayList<String>());
+					
+					String nlr = p.getNaturalLanguageRepresentation();
+//					nlr = year.replaceAll("YEAR");
+//					nlr = nlr.replaceAll("(-)?\\d+(\\.\\d*)?", "NUMBER");
+					
+					patterns.get(mapping.getProperty().getUri()).add(nlr);
+				}
+    		}
+    	}
+    	
+    	for ( Map.Entry<String, List<String>> pattern : patterns.entrySet() ) {
+    		
+    		System.out.println(pattern.getKey());
+    		Collections.sort(pattern.getValue());
+    		for( String p : pattern.getValue() ) 
+    			System.out.println("\t"+p);
+    		
+    		System.out.println(pattern.getValue().size());
+    	}
+	}
+    
+    
     private PatternMappingManager() {
         
-        mappingsInDatabases.put(
-                DEFAULT_DATABASE, 
-                SerializationManager.getInstance().deserializePatternMappings(PATTERN_MAPPING_FOLDER));
+//        mappingsInDatabases.put(
+//                DEFAULT_DATABASE, 
+//                SerializationManager.getInstance().deserializePatternMappings(PATTERN_MAPPING_FOLDER));
     }
     
     public static PatternMappingManager getInstance(){
