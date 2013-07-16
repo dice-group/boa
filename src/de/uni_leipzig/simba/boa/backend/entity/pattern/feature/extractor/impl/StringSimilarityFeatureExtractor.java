718f4dc8a85dc4d99958947485833f89ca25263e
Daniel Gerber <dgerber@informatik.uni-leipzig.de>
2013-07-16 14:17:22 +0200

    configurable string sim  feature

--- src/de/uni_leipzig/simba/boa/backend/entity/pattern/feature/extractor/impl/StringSimilarityFeatureExtractor.java
+++ src/de/uni_leipzig/simba/boa/backend/entity/pattern/feature/extractor/impl/StringSimilarityFeatureExtractor.java
@@ -67,7 +67,7 @@ public class StringSimilarityFeatureExtractor extends AbstractFeatureExtractor {
 		String query = String.format("SELECT ?label " +
 				"{ <%s> rdfs:label ?label . FILTER(lang(?label) = '%s') }", uri, NLPediaSettings.BOA_LANGUAGE); 
 		
-		QueryEngineHTTP qexecProperty = new QueryEngineHTTP("http://[2001:638:902:2010:0:168:35:138]/sparql", query);
+		QueryEngineHTTP qexecProperty = new QueryEngineHTTP(NLPediaSettings.getSetting("dbpediaSparqlEndpoint"), query);
         qexecProperty.addDefaultGraph("http://boa.dbpedia.org");
 
         ResultSet results = qexecProperty.execSelect();
